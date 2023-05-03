a41b2bec8e8fd5b241ceb6a31a2ff08fddc6f813
bcho892 <bcho892@aucklanduni.ac.nz>
2023-05-04 11:16:50 +1200

    feat: got top bar appearance matching design

--- app/src/main/java/com/application/project2java/MainActivity.java
+++ app/src/main/java/com/application/project2java/MainActivity.java
@@ -2,11 +2,12 @@ package com.application.project2java;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.Activity;
 import android.os.Bundle;
 
 import com.example.project2java.R;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends Activity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
