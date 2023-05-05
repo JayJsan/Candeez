d08f01d28a8cca5b36e23b4e3aa1b018e44d9d86
bcho892 <bcho892@aucklanduni.ac.nz>
2023-05-05 20:59:39 +1200

    feat: add topbar to listview

---
+++ app/src/main/java/com/application/project2java/ListActivity.java
@@ -0,0 +1,16 @@
+package com.application.project2java;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.project2java.R;
+
+public class ListActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_list);
+    }
+}
