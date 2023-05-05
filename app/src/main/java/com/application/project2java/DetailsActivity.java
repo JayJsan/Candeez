ab4d019ba753e1c7e93b06c7ad4ab4a81f60c164
bcho892 <AKLbc70@gmail.com>
2023-05-06 11:58:18 +1200

    fix: stop bottom nav from covering view in DetailsActivity

--- app/src/main/java/com/application/project2java/DetailsActivity.java
+++ app/src/main/java/com/application/project2java/DetailsActivity.java
@@ -1,16 +1,19 @@
 package com.application.project2java;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.FragmentActivity;
 
 import android.os.Bundle;
 
 import com.example.project2java.R;
 
-public class DetailsActivity extends AppCompatActivity {
+public class DetailsActivity extends FragmentActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_details);
+        BottomNavigationUtils.setupBottomNavigationView(this);
+        BottomNavigationUtils.setCurrentItem(this);
     }
 }
