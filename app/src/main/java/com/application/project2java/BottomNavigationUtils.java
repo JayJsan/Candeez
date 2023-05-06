867a44965d00f8462b6ddf935c1650ea5ae8f322
bcho892 <AKLbc70@gmail.com>
2023-05-06 15:41:19 +1200

    fix: check correct item on details activity

--- app/src/main/java/com/application/project2java/BottomNavigationUtils.java
+++ app/src/main/java/com/application/project2java/BottomNavigationUtils.java
@@ -50,7 +50,7 @@ public class BottomNavigationUtils {
         int selectedItemIndex;
         if (activity instanceof MainActivity) {
             selectedItemIndex = 0;
-        } else if (activity instanceof ListActivity) {
+        } else if (activity instanceof ListActivity || activity instanceof DetailsActivity) {
             selectedItemIndex = 1;
         } else {
             selectedItemIndex = 0;
