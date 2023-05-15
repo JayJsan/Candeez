4225c87df62547424f6fb2679fb8fd4f067fa395
bcho892 <AKLbc70@gmail.com>
2023-05-15 20:15:15 +1200

    test: add query util unit tests

---
+++ app/src/main/java/com/application/project2java/QueryUtils.java
@@ -0,0 +1,21 @@
+package com.application.project2java;
+
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+public final class QueryUtils {
+    public static String[] joinCategories(List<CategoryName> categories) {
+        List<String> categoryStrings = new ArrayList<>();
+        for (CategoryName category : categories) {
+            categoryStrings.add(category.toString());
+        }
+        return categoryStrings.toArray(new String[categories.size()]);
+    }
+
+    public static String formatQueryWithArray(int argCount, String query) {
+        return query.replace("?", String.join(",", Collections.nCopies(argCount, "?")));
+
+    }
+}
---
+++ app/src/test/java/com/application/project2java/QueryUtilsTest.java
@@ -0,0 +1,30 @@
+package com.application.project2java;
+
+import static com.application.project2java.ItemContract.ItemEntry.COLUMN_CATEGORY;
+import static com.application.project2java.ItemContract.ItemTable.TABLE_NAME;
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+public class QueryUtilsTest {
+
+    @Test
+    public void testJoinCategories() {
+        List<CategoryName> categories = new ArrayList<>();
+        categories.add(CategoryName.Gummies);
+        categories.add(CategoryName.Hard_Candy);
+        String[] result = QueryUtils.joinCategories(categories);
+        assertArrayEquals(new String[]{"Gummies", "Hard_Candy"}, result);
+    }
+
+    @Test
+    public void testFormatArrayQuery(){
+       String result = QueryUtils.formatQueryWithArray(4,QueryProvider.CATEGORY_ITEM_QUERY);
+       assertEquals("SELECT * FROM " + TABLE_NAME + " WHERE " + COLUMN_CATEGORY + " IN (?,?,?,?)", result);
+
+    }
+}
