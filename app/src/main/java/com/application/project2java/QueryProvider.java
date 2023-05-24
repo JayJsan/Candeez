7c339c2fef2ab3365821282ffe6ff665b5f58fec
bcho892 <AKLbc70@gmail.com>
2023-05-24 23:46:13 +1200

    feat: add query for item with name

--- app/src/main/java/com/application/project2java/QueryProvider.java
+++ app/src/main/java/com/application/project2java/QueryProvider.java
@@ -3,6 +3,7 @@ package com.application.project2java;
 import static com.application.project2java.ItemContract.ItemEntry.COLUMN_CART_QUANTITY;
 import static com.application.project2java.ItemContract.ItemEntry.COLUMN_CATEGORY;
 import static com.application.project2java.ItemContract.ItemEntry.COLUMN_IS_FAVOURITE;
+import static com.application.project2java.ItemContract.ItemEntry.COLUMN_NAME;
 import static com.application.project2java.ItemContract.ItemEntry.COLUMN_VIEW_COUNT;
 import static com.application.project2java.ItemContract.ItemTable.TABLE_NAME;
 
@@ -20,6 +21,7 @@ public final class QueryProvider {
     public static final String ALL_ITEMS_QUERY = "SELECT * FROM " + TABLE_NAME;
     public static final String CATEGORY_ITEM_QUERY = "SELECT * FROM " + TABLE_NAME + " WHERE " + COLUMN_CATEGORY + " IN (?)";
     public static final String CATEGORY_ITEM_FREQUENCY_QUERY = "SELECT COUNT(*) FROM " + TABLE_NAME + " WHERE " + COLUMN_CATEGORY + " IN (?)";
+    public static final String ITEM_BY_NAME_QUERY = "SELECT * FROM " + TABLE_NAME + " WHERE " + COLUMN_NAME + " = ? LIMIT 1";
 
     private QueryProvider() {
     }
