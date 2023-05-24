32ef0096df1cc14aff3fa27bb10060b51b780574
bcho892 <AKLbc70@gmail.com>
2023-05-25 11:20:00 +1200

    feat: allow removing items

--- app/src/main/java/com/application/project2java/FavouritesActivity.java
+++ app/src/main/java/com/application/project2java/FavouritesActivity.java
@@ -12,6 +12,9 @@ import java.util.List;
 
 public class FavouritesActivity extends FragmentActivity {
     DataProvider dataProvider;
+    DataMutator dataMutator;
+    List<ItemModel> favouriteItems;
+    CompactListAdapter favouritesAdapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -21,16 +24,27 @@ public class FavouritesActivity extends FragmentActivity {
         BottomNavigationUtils.setCurrentItem(this);
 
         dataProvider = App.getDataProvider();
+        dataMutator = App.getDataMutator();
+
+        dataMutator.addDatabaseWriteListener(this::updateItems);
         dataProvider.open();
         setupFavouritesRecyclerView();
         dataProvider.close();
     }
 
+    private void updateItems() {
+        dataProvider.open();
+        favouriteItems = dataProvider.getFavouriteItems();
+        dataProvider.close();
+        favouritesAdapter.setItems(favouriteItems);
+
+    }
+
     private void setupFavouritesRecyclerView() {
         RecyclerView favouritesRecyclerView = this.findViewById(R.id.recycler_view_favourites);
-        List<ItemModel> items = dataProvider.getFavouriteItems();
-        CompactListAdapter compactListAdapter = new CompactListAdapter(items);
-        favouritesRecyclerView.setAdapter(compactListAdapter);
+        favouriteItems = dataProvider.getFavouriteItems();
+        favouritesAdapter = new CompactListAdapter(favouriteItems);
+        favouritesRecyclerView.setAdapter(favouritesAdapter);
         LinearLayoutManager productLayoutManager = new LinearLayoutManager(this);
         favouritesRecyclerView.setLayoutManager(productLayoutManager);
     }
