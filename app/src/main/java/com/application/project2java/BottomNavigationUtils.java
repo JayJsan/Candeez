d45a84875636df1edcddb1253f379e424b9dba32
bcho892 <AKLbc70@gmail.com>
2023-05-26 09:23:10 +1200

    fix: destroy activity on navigation

--- app/src/main/java/com/application/project2java/BottomNavigationUtils.java
+++ app/src/main/java/com/application/project2java/BottomNavigationUtils.java
@@ -42,6 +42,7 @@ public class BottomNavigationUtils {
     private static void navigateToDestination(FragmentActivity activity, Class<?> destinationClass, int itemId) {
         Intent intent = new Intent(activity, destinationClass);
         activity.startActivity(intent);
+        activity.finish();
     }
 
     public static void setCurrentItem(FragmentActivity activity) {
