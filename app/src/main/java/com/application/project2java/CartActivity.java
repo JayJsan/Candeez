305db348b6bb9a0b96a85df0618df8d3bf36be8e
bcho892 <AKLbc70@gmail.com>
2023-05-26 21:23:19 +1200

    feat: got base recycler view working for cart

--- app/src/main/java/com/application/project2java/CartActivity.java
+++ app/src/main/java/com/application/project2java/CartActivity.java
@@ -2,17 +2,38 @@ package com.application.project2java;
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.fragment.app.FragmentActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
 
 import com.example.project2java.R;
 
+import java.util.List;
+
 public class CartActivity extends FragmentActivity {
+
+    private CartAdapter cartAdapter;
+    private List<ItemModel> items;
+    private DataProvider dataProvider;
+    private DataMutator dataMutator;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_cart);
+
+        RecyclerView cartRecyclerView = findViewById(R.id.cart_recycler_view);
+        dataProvider = App.getDataProvider();
+        dataProvider.open();
+        items = dataProvider.getAllItems();
+        dataProvider.close();
+        cartAdapter = new CartAdapter(items);
+        cartRecyclerView.setAdapter(cartAdapter);
+        LinearLayoutManager cartLayoutManager = new LinearLayoutManager(this);
+        cartRecyclerView.setLayoutManager(cartLayoutManager);
+
+
         BottomNavigationUtils.setupBottomNavigationView(this);
         BottomNavigationUtils.setCurrentItem(this);
     }
---
+++ app/src/main/java/com/application/project2java/CartAdapter.java
@@ -0,0 +1,90 @@
+package com.application.project2java;
+
+
+import android.graphics.Bitmap;
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.core.content.ContextCompat;
+import androidx.core.graphics.drawable.DrawableCompat;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.example.project2java.R;
+import com.google.android.material.imageview.ShapeableImageView;
+
+import java.util.List;
+
+public class CartAdapter extends RecyclerView.Adapter<CartAdapter.ViewHolder> {
+    private List<ItemModel> items;
+
+    public CartAdapter(List<ItemModel> items) {
+        this.items = items;
+    }
+
+    public void setItems(List<ItemModel> items) {
+        this.items = items;
+        notifyDataSetChanged();
+    }
+
+    @NonNull
+    @Override
+    public CartAdapter.ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        View v = LayoutInflater.from(parent.getContext())
+                .inflate(R.layout.cart_list_item, parent, false);
+        return new ViewHolder(v);
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull CartAdapter.ViewHolder holder, int position) {
+        ItemModel item = items.get(position);
+        String name = item.getName();
+
+        holder.textViewPrice.setText("$" + item.getPrice());
+        holder.textViewName.setText(name);
+        holder.itemName = name;
+
+        ResourceUtils.getImageBitmapAsync(item.getImageUris().get(0), new ResourceUtils.BitmapCallback() {
+
+            @Override
+            public void onBitmapLoaded(Bitmap bitmap) {
+                holder.imageView.setImageBitmap(bitmap);
+            }
+
+            @Override
+            public void onBitmapFailed(Exception e) {
+                holder.imageView.setImageResource(R.drawable.baseline_image_not_supported_24);
+                DrawableCompat.setTint(
+                        DrawableCompat.wrap(holder.imageView.getDrawable()),
+                        ContextCompat.getColor(App.getAppContext(), R.color.md_theme_light_primaryContainer)
+                );
+                Log.d("DEBUG", e.getLocalizedMessage());
+            }
+        });
+    }
+
+    @Override
+    public int getItemCount() {
+        return items.size();
+    }
+
+    public static class ViewHolder extends RecyclerView.ViewHolder {
+        String itemName;
+        TextView textViewName;
+        TextView textViewPrice;
+        ShapeableImageView imageView;
+
+        public ViewHolder(View v) {
+            super(v);
+            // Define click listener for the ViewHolder's View.
+            v.setOnClickListener(v1 -> ListItemUtils.navigateToDetails(itemName));
+            textViewName = v.findViewById(R.id.product_name);
+            textViewPrice = v.findViewById(R.id.product_price);
+            imageView = v.findViewById(R.id.image_cart_item);
+        }
+
+    }
+}
