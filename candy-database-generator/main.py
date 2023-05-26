412e36d1727a6a5883acbadbfd5b7fdbae79877d
Job <jsan502@aucklanduni.ac.nz>
2023-05-26 22:54:51 +1200

    fix: app appends to csv

--- candy-database-generator/main.py
+++ candy-database-generator/main.py
@@ -1,4 +1,5 @@
 import csv
+import os
 
 def extract_product_data(text):
     lines = text.split('/')
@@ -9,20 +10,26 @@ def extract_product_data(text):
     return price, category, name, description
 
 def write_to_csv(data, filename):
-    with open(filename, 'w', newline='') as csvfile:
+    file_exists = os.path.isfile(filename)
+    with open(filename, 'a', newline='') as csvfile:
         writer = csv.writer(csvfile)
-        writer.writerow(['price', 'image_uri','category','view_count','is_favourite','cart_quantity','name', 'description'])
+        if not file_exists:
+            writer.writerow(['price', 'image_uri','category','view_count','is_favourite','cart_quantity','name', 'description'])
         writer.writerow(data)
-    print(f"CSV file '{filename}' generated successfully.")
+    print(f"CSV file '{filename}' generated/edited successfully.")
 
-# Take input from the user
-input_text = input("Enter the product details (price, category, name, description) seperated by a forward slash:\n")
+while True:
+    # Take input from the user
+    input_text = input("Enter the product details (price, category, name, description) seperated by a forward slash:\n")
 
-# Extract the product data
-product_price, product_category, product_name, product_description = extract_product_data(input_text)
+    if input_text.lower() == 'q':
+        break
 
-# Specify the filename for the CSV file
-filename = 'candeez_database.csv'
+    # Extract the product data
+    product_price, product_category, product_name, product_description = extract_product_data(input_text)
 
-# Write the product data to a CSV file
-write_to_csv([product_price, "", product_category, 0, 0,product_name, product_description], filename)
+    # Specify the filename for the CSV file
+    filename = 'candeez_database.csv'
+
+    # Write the product data to a CSV file
+    write_to_csv([product_price, "", product_category, 0, 0,product_name, product_description], filename)
